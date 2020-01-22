a1a518afe10be875d7e53428c18ba996974739df
Mike Liao <mike.hm.liao@gmail.com>
2020-01-22 21:22:55 +0800

    added notes commands

---
+++ NotesApp/ViewModel/Commands/NewNoteCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace NotesApp.ViewModel.Commands
+{
+    public class NewNoteCommand : ICommand
+    {
+        public NotesViewModel VM { get; set; }
+
+        public event EventHandler CanExecuteChanged;
+
+        public NewNoteCommand(NotesViewModel vm)
+        {
+            VM = VM;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            //TODO: 
+        }
+    }
+}
