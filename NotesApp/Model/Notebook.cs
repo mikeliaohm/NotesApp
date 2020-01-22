081184f364fda1ebbb22a578f0dab26f37abfb5c
Mike Liao <mike.hm.liao@gmail.com>
2020-01-22 08:28:03 +0800

    created model classes

---
+++ NotesApp/Model/Notebook.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesApp.Model
+{
+    class Notebook
+    {
+		private int userId;
+
+		public int UserId
+		{
+			get { return userId; }
+			set { userId = value; }
+		}
+
+		private string name;
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+
+	}
+}
