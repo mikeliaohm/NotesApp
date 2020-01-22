7609f0eba2b7607a5b585d58376e926e1a86bdf3
Mike Liao <mike.hm.liao@gmail.com>
2020-01-22 21:11:35 +0800

    added login and register commands

---
+++ NotesApp/ViewModel/Commands/LoginCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace NotesApp.ViewModel.Commands
+{
+    public class LoginCommand : ICommand
+    {
+        public LoginViewModel VM { get; set; }
+
+        public event EventHandler CanExecuteChanged;
+
+        public LoginCommand(LoginViewModel vm)
+        {
+            VM = vm;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            //TODO:
+        }
+    }
+}
---
+++ NotesApp/ViewModel/Commands/RegisterCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace NotesApp.ViewModel.Commands
+{
+    public class RegisterCommand : ICommand
+    {
+        public LoginViewModel VM { get; set; }
+
+        public RegisterCommand(LoginViewModel vm)
+        {
+            VM = vm;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            //TODO: 
+        }
+    }
+}
