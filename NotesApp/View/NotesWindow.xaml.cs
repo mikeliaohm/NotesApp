e36751da849498e4cea6c3cbffcd5f88a5fc7fed
Mike Liao <mike.hm.liao@gmail.com>
2020-01-23 09:25:24 +0800

    selected text supports font weight style matching

--- NotesApp/View/NotesWindow.xaml.cs
+++ NotesApp/View/NotesWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -22,8 +23,6 @@ namespace NotesApp.View
     public partial class NotesWindow : Window
     {
         SpeechRecognitionEngine recognizer;
-
-        bool isRecognizing = false;
 
         public NotesWindow()
         {
@@ -58,20 +57,25 @@ namespace NotesApp.View
 
         private void BoldButton_Click(object sender, RoutedEventArgs e)
         {
-            contentRichTextBox.Selection.ApplyPropertyValue(Inline.FontWeightProperty, FontWeights.Bold);
+            bool isButtonEnabled = (sender as ToggleButton).IsChecked ?? false;
+
+            if(isButtonEnabled)
+                contentRichTextBox.Selection.ApplyPropertyValue(Inline.FontWeightProperty, FontWeights.Bold);
+            else
+                contentRichTextBox.Selection.ApplyPropertyValue(Inline.FontWeightProperty, FontWeights.Normal);
         }
 
         private void SpeechButton_Click(object sender, RoutedEventArgs e)
         {
-            if(!isRecognizing)
+            bool isButtonEnabled = (sender as ToggleButton).IsChecked ?? false;
+
+            if(!isButtonEnabled)
             {
                 recognizer.RecognizeAsync(RecognizeMode.Multiple);
-                isRecognizing = true;
             }
             else
             {
                 recognizer.RecognizeAsyncStop();
-                isRecognizing = false;
             }
         }
 
@@ -80,6 +84,12 @@ namespace NotesApp.View
             string recognizedText = e.Result.Text;
 
             contentRichTextBox.Document.Blocks.Add(new Paragraph(new Run(recognizedText)));
+        }
+
+        private void contentRichTextBox_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            var selectedState = contentRichTextBox.Selection.GetPropertyValue(Inline.FontWeightProperty);
+            BoldButton.IsChecked = (selectedState != DependencyProperty.UnsetValue) && (selectedState.Equals(FontWeights.Bold));
         }
     }
 }
